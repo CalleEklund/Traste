cf67150d0abe87fbcb5acf7ed23f53a76279128b
Rasmus Kramare <rasmus.kramare@live.se>
2022-03-03 11:59:27 +0100

    Added eslint config. Comments in header.js

--- app/traste/src/components/Header.js
+++ app/traste/src/components/Header.js
@@ -1,32 +1,39 @@
-import React from "react";
-import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
-import Toolbar from "@mui/material/Toolbar";
-import Typography from "@mui/material/Typography";
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
-import logo from "../assets/traste_logo.png";
-import KeyboardBackspaceIcon from "@mui/icons-material/KeyboardBackspace";
-import {Colors} from "../assets/Colors"
+import logo from '../assets/traste_logo.png';
+import KeyboardBackspaceIcon from '@mui/icons-material/KeyboardBackspace';
 
+import {Colors} from '../assets/Colors';
 
-function Header(props) {
+/**
+ * The main header for the app. Used to navigate the app.
+ * @param {goBackHandler} props Contains the handler for navigating back
+ * in the document
+ * @return {AppBar} with logo and title.
+ */
+function Header({goBackHandler}) {
   return (
-  
-      <AppBar 
-        data-testid="header"
-        position = "sticky"
-        sx={{ textAlign: "center", backgroundColor: Colors.trasteNavyBlue }}
-      >
-        <Toolbar>
-          <KeyboardBackspaceIcon fontSize="large" onClick={props.goBackHandler} />
-
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            TRASTE
-          </Typography>
-          <img src={logo} alt="traste logo" style={{ height: "40px" }} />
-        </Toolbar>
-      </AppBar>
-
+    <AppBar
+      data-testid='header'
+      position='sticky'
+      sx={{textAlign: 'center', backgroundColor: Colors.trasteNavyBlue}}
+    >
+      <Toolbar>
+        <KeyboardBackspaceIcon fontSize='large' onClick={goBackHandler} />
+        <Typography variant='h6' component='div' sx={{flexGrow: 1}}>
+          TRASTE
+        </Typography>
+        <img src={logo} alt='traste logo' style={{height: '40px'}} />
+      </Toolbar>
+    </AppBar>
   );
 }
+
+Header.propTypes = {
+  goBackHandler: PropTypes.func.isRequired,
+};
+
 export default Header;
