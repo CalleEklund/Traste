b1f147104f07590284c126c1dc1c4ae00050b62f
Rasmus Kramare <rasmus.kramare@live.se>
2022-03-04 16:57:10 +0100

    Commented and lintyfied.

--- app/traste/src/components/MaterialField.js
+++ app/traste/src/components/MaterialField.js
@@ -1,40 +1,56 @@
-import React from "react";
+import React from 'react';
 
-import { TextField, InputAdornment } from "@mui/material";
+import {TextField, InputAdornment} from '@mui/material';
 
-function MaterialField({ label, value, onChange, error }) {
+/**
+ * Custom TextField for displaying a procentage for a specified material.
+ * @param {*} label Name of field.
+ * @param {*} value Starting value.
+ * @param {*} onChange Handler.
+ * @param {*} error What will be shown for the error.
+ * @return {TextField} Rendred field.
+ */
+function MaterialField({label, value, onChange, error}) {
   return (
     <TextField
       label={label}
       InputProps={{
         endAdornment: (
-          <InputAdornment position="start" disableTypography={true}>
+          <InputAdornment position='start' disableTypography={true}>
             %
           </InputAdornment>
         ),
       }}
-      variant="outlined"
-      type="number"
+      variant='outlined'
+      type='number'
       onFocus={(event) => {
         event.target.select();
       }}
       onBlur={(evt) => {
-        if (evt.target.value === "") {
+        if (evt.target.value === '') {
           onChange(0);
         }
       }}
       value={value}
       onChange={onChange}
       sx={{
-        backgroundColor: "rgba(255,255,255,0.3)",
+        backgroundColor: 'rgba(255,255,255,0.3)',
       }}
       inputProps={{
-        style: { textAlign: "right", fontSize: 16 },
-        "data-testid": "materialfield",
+        'style': {textAlign: 'right', fontSize: 16},
+        'data-testid': 'materialfield',
       }}
       error={!!error}
-      //helperText={error ? error.message : null}
+      // helperText={error ? error.message : null}
     />
   );
 }
+
+MaterialField.propTypes = {
+  label: PropTypes.func.isRequired,
+  value: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.func.isRequired,
+};
+
 export default MaterialField;
