653f138fcd228e66bdeb2a3dd4e8c2ba6cadd487
Rasmus Kramare <rasmus.kramare@live.se>
2022-03-04 15:44:05 +0100

    Commented and ESlintyfied.

--- app/traste/src/components/Inputfield.js
+++ app/traste/src/components/Inputfield.js
@@ -1,23 +1,41 @@
-import React from "react";
-import { TextField } from "@mui/material";
+import React from 'react';
+import {TextField} from '@mui/material';
 
-function Inputfield({ label, value, onChange, error, type }) {
+/**
+ * Custom TextField used for different data in the RenderPage.
+ * @param {*} label Name of field.
+ * @param {*} value Starting value.
+ * @param {*} onChange Handler.
+ * @param {*} error What will be shown for the error.
+ * @param {*} type What type of values. Usally number or text.
+ * @return {TextField} Rendred field
+ */
+function Inputfield({label, value, onChange, error, type}) {
   return (
     <TextField
       label={label}
-      variant="outlined"
+      variant='outlined'
       sx={{
-        marginTop: "15px",
-        backgroundColor: "rgba(255,255,255,0.3)",
-        width: "90vw",
+        marginTop: '15px',
+        backgroundColor: 'rgba(255,255,255,0.3)',
+        width: '90vw',
       }}
       value={value}
       type={type}
       onChange={onChange}
-      inputProps={{ "data-testid": "inputfield" }}
+      inputProps={{'data-testid': 'inputfield'}}
       error={!!error}
-      //helperText={error ? error.message : null}
+      // helperText={error ? error.message : null}
     />
   );
 }
+
+Inputfield.propTypes = {
+  label: PropTypes.func.isRequired,
+  value: PropTypes.func.isRequired,
+  onChange: PropTypes.func.isRequired,
+  error: PropTypes.func.isRequired,
+  type: PropTypes.func.isRequired,
+};
+
 export default Inputfield;
