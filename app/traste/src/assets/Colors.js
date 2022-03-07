6705a8ccb1cb3c9423ad1088f5d34175bca14002
Rasmus Kramare <rasmus.kramare@live.se>
2022-03-07 10:03:18 +0100

    Lintyfied

--- app/traste/src/assets/Colors.js
+++ app/traste/src/assets/Colors.js
@@ -1,9 +1,9 @@
 export const Colors = {
-    trasteNavyBlue: "#103849",
-    trasteGreen: "#80ed99",
-    trasteTeal: "#00a8ab",
-    trasteDadada: "#dadada", 
-    trasteDarkPurple: "#170055",
-    trasteBlue: "#3e00ff",
-    trastePurple: "#ae00fb",
+  trasteNavyBlue: '#103849',
+  trasteGreen: '#80ed99',
+  trasteTeal: '#00a8ab',
+  trasteDadada: '#dadada',
+  trasteDarkPurple: '#170055',
+  trasteBlue: '#3e00ff',
+  trastePurple: '#ae00fb',
 };
