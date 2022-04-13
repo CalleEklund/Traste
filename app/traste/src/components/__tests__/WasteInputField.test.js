248ac5cacb30e5bb2a5b0525b4ec32229945b99a
Erik <erikbirgersson98@gmail.com>
2022-04-13 16:27:02 +0200

    "Eslint warnings fixed"

--- app/traste/src/components/__tests__/WasteInputField.test.js
+++ app/traste/src/components/__tests__/WasteInputField.test.js
@@ -1,6 +1,5 @@
 import {cleanup} from '@testing-library/react';
 import '@testing-library/jest-dom';
-import renderer from 'react-test-renderer';
 import React from 'react';
 import {configure, shallow} from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
@@ -15,15 +14,17 @@ afterEach(() => {
 const onlyNumbers = (score) => !isNaN(parseInt(score)) && isFinite(score);
 
 test('Check so that WasteInputField renders', () => {
-  const control = jest.fn();  
-  const wrapper = shallow(<WasteInputField control={control} onlyNumbers={onlyNumbers} />);
+  const control = jest.fn();
+  const wrapper = shallow(<WasteInputField control={control}
+    onlyNumbers={onlyNumbers} />);
 
   expect(wrapper).toHaveLength(1);
 });
 
 test('Matches snapshot', () => {
-    const control = jest.fn();
-  
-    const wrapper = shallow(<WasteInputField control={control} onlyNumbers={onlyNumbers} />);
-    expect(wrapper).toMatchSnapshot();
-  });
+  const control = jest.fn();
+
+  const wrapper = shallow(<WasteInputField control={control}
+    onlyNumbers={onlyNumbers} />);
+  expect(wrapper).toMatchSnapshot();
+});
